c65eef9a442db3d76cc1d6410a3192c9abb0d70b
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-12 18:42:39 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -6,3 +6,14 @@ collection = [1, 2, 3, 4]
 my_each(collection) do |i|
   puts i
 end
+
+def hello_t(array)
+  i = 0
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+ 
+  array
+end
